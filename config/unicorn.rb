47fb35540f16027990a1895cf6b01068fb7d5000
Alex Stoica <alex_stoicajr@yahoo.co.uk>
2013-08-23 19:34:31 +0300

    added new config file for unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,6 +1,9 @@
 timeout 30
 worker_processes 5
 preload_app true
+pid "/root/parserizer/log/unicorn.pid"
+stderr_path "/root/parserizer/log/unicorn.stderr.log"
+stdout_path "/root/parserizer/log/unicorn.stdout.log"
 
 before_exec do |server|
   ENV['BUNDLE_GEMFILE'] = File.expand_path('../Gemfile', File.dirname(__FILE__))
