e0b9429962948c1e36ed6fe23597282f19ee0c2b
Alex Stoica <alex_stoicajr@yahoo.co.uk>
2013-08-26 12:51:09 +0300

    now leaving images and links

--- parser.rb
+++ parser.rb
@@ -9,7 +9,7 @@ class Parser
 	def initialize( url )
 		begin
 			source = open( url ).read
-			body = Readability::Document.new(source)
+			body = Readability::Document.new(source , :tags => %w[div p img a] , :attributes => %w[href src] )
 			@parsed = body.content
 			@images = body.images
 		rescue Exception => e
